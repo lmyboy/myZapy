5fd9b9950b5460765c61585ea05d12e0cdaec93f
“lmyboy” <lucasldc1@gmail.com>
2019-07-29 14:27:37 -0300

    views

--- app.js
+++ app.js
@@ -4,12 +4,33 @@
 // Adicionando o Express ao projeto, é preciso instalar antes com: 
 // npm i express --save -> é para escrever no package.json
 const express = require('express');
+const path = require('path');
+
 
 const app = express();
 
+app.use('/views', express.static('views')); // permitir acesso estático a pasta
+
+// ======= FUNCTIONS
+
+const getViewPath = (view) => {
+    return path.join(__dirname, `./views/${view}/${view}.html`);
+}
+
+// ======= VIEWS
 app.get('/', (req, res)=> {
-    res.send('yay');
+    res.sendFile(getViewPath('home'));
 });
+
+app.get('/:view', (req, res) => {
+    res.sendFile(getViewPath(req.params.view), err => {
+        if(err){
+            res.send('404');
+        }
+    });
+})
+
+// ====== Server run
 
 app.listen(2018, ()=> {
     console.log('Rôdando!\n');
